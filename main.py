da71ee25a4d0a251cce0e270bcfdc328997e7cfc
Anilforextra <anilforextra@gmail.com>
2026-06-22 00:13:51 +0545

    Add screen to world conversions helpers.

--- main.py
+++ main.py
@@ -13,16 +13,27 @@ pygame.init()
 GRID_SIZE = 15
 GRID_MAJOR_EVERY = 60
 
+CAM_POS_X = 0
+CAM_POS_Y = 0
 
-def draw_grid(screen, cam_x, cam_y, width, height):
-    start_world_x, start_world_y = -cam_x, -cam_y
+
+def world_to_screen(world_pos):
+    return (world_pos[0] + CAM_POS_X, world_pos[1] + CAM_POS_Y)
+
+
+def screen_to_world(screen_pos):
+    return (screen_pos[0] - CAM_POS_X, screen_pos[1] - CAM_POS_Y)
+
+
+def draw_grid(screen, width, height):
+    start_world_x, start_world_y = screen_to_world((0, 0))
 
     # Vertical lines.
     start_x = int(start_world_x // GRID_SIZE) * GRID_SIZE
     end_x = int(start_world_x + width) + GRID_SIZE
 
     for world_x in range(start_x, end_x, GRID_SIZE):
-        sx = world_x + cam_x
+        sx, _ = world_to_screen((world_x, 0))
         if 0 <= sx <= width:
             color = GRID_COLOR if world_x % GRID_MAJOR_EVERY == 0 else GRID_COLOR_DARK
             pygame.draw.line(screen, color, (sx, 0), (sx, height))
@@ -32,14 +43,14 @@ def draw_grid(screen, cam_x, cam_y, width, height):
     end_y = int(start_world_y + height) + GRID_SIZE
 
     for world_y in range(start_y, end_y, GRID_SIZE):
-        sy = world_y + cam_y
+        _, sy = world_to_screen((0, world_y))
         if 0 <= sy <= height:
             color = GRID_COLOR if world_y % GRID_MAJOR_EVERY == 0 else GRID_COLOR_DARK
             pygame.draw.line(screen, color, (0, sy), (width, sy))
 
 
 def main():
-    global WIDTH, HEIGHT
+    global WIDTH, HEIGHT, CAM_POS_X, CAM_POS_Y
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
     pygame.display.set_caption("Wires Game Engine")
@@ -47,7 +58,7 @@ def main():
     sdl_window = Window.from_display_module()
     sdl_window.maximize()
 
-    cam_x, cam_y = WIDTH // 2, HEIGHT // 2
+    CAM_POS_X, CAM_POS_Y = WIDTH // 2, HEIGHT // 2
 
     panning = False
     last_mouse_pos = pygame.mouse.get_pos()
@@ -56,11 +67,11 @@ def main():
     while running:
         current_mouse_pos = pygame.mouse.get_pos()
         if panning:
-            cam_x += current_mouse_pos[0] - last_mouse_pos[0]
-            cam_y += current_mouse_pos[1] - last_mouse_pos[1]
+            CAM_POS_X += current_mouse_pos[0] - last_mouse_pos[0]
+            CAM_POS_Y += current_mouse_pos[1] - last_mouse_pos[1]
 
         last_mouse_pos = current_mouse_pos
-        world_mouse = (current_mouse_pos[0] - cam_x, current_mouse_pos[1] - cam_y)
+        world_mouse = screen_to_world(current_mouse_pos)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -77,7 +88,7 @@ def main():
 
         screen.fill(BG_COLOR)
 
-        draw_grid(screen, cam_x, cam_y, WIDTH, HEIGHT)
+        draw_grid(screen, WIDTH, HEIGHT)
 
         pygame.display.flip()
 
